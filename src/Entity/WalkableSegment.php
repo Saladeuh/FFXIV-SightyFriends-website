a2750e5eab1ca23c164fee7f7389b9e4d6f62eb1
Saladeuh <emilienmonet10@gmail.com>
2024-03-02 23:43:14 +0100

    feat: add segments as apiresource

--- src/Entity/WalkableSegment.php
+++ src/Entity/WalkableSegment.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\WalkableSegmentRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: WalkableSegmentRepository::class)]
+#[ApiResource]
 class WalkableSegment
 {
     #[ORM\Id]
