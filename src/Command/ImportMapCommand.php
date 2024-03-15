6c204888be39de67d5d002b2ecbf832ddd79f1a6
Saladeuh <emilienmonet10@gmail.com>
2024-03-15 23:21:02 +0100

    cs fix

--- src/Command/ImportMapCommand.php
+++ src/Command/ImportMapCommand.php
@@ -6,9 +6,7 @@ use App\Entity\Map;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -19,36 +17,36 @@ class ImportMapCommand extends Command
 {
     public function __construct(
         private EntityManagerInterface $entityManager
-    )
-    {
+    ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        if (($handleMaps = fopen(__DIR__."/Map.csv", "r")) !== FALSE && ($handleNames = fopen(__DIR__."/PlaceName.csv", "r")) !== FALSE) {
-            $maps=[];
-            $names=[];
-            while (($mapRow = fgetcsv($handleMaps)) !== FALSE) {
+        if (($handleMaps = fopen(__DIR__.'/Map.csv', 'r')) !== false && ($handleNames = fopen(__DIR__.'/PlaceName.csv', 'r')) !== false) {
+            $maps = [];
+            $names = [];
+            while (($mapRow = fgetcsv($handleMaps)) !== false) {
                 $maps[] = $mapRow;
             }
-            while (($nameRow = fgetcsv($handleNames)) !== FALSE) {
+            while (($nameRow = fgetcsv($handleNames)) !== false) {
                 $names[] = $nameRow;
             }
             fclose($handleMaps);
             fclose($handleNames);
             foreach ($maps as $map) {
                 $nameId = $map[13];
-                if(is_numeric($nameId)) {
+                if (is_numeric($nameId)) {
                     $placeName = $names[$nameId][1];
                     $io->writeln($placeName);
-                    $mapEntity=(new Map())->setGameId((int)$map[0])->setPlaceName($placeName);
+                    $mapEntity = (new Map())->setGameId((int) $map[0])->setPlaceName($placeName);
                     $this->entityManager->persist($mapEntity);
                 }
             }
             $this->entityManager->flush();
         }
+
         return Command::SUCCESS;
     }
 }
--- src/Repository/WalkableRepository.php
+++ src/Repository/WalkableRepository.php
@@ -21,28 +21,28 @@ class WalkableRepository extends ServiceEntityRepository
         parent::__construct($registry, WalkablePoint::class);
     }
 
-//    /**
-//     * @return Walkable[] Returns an array of Walkable objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('w')
-//            ->andWhere('w.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('w.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Walkable[] Returns an array of Walkable objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('w')
+    //            ->andWhere('w.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('w.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Walkable
-//    {
-//        return $this->createQueryBuilder('w')
-//            ->andWhere('w.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Walkable
+    //    {
+    //        return $this->createQueryBuilder('w')
+    //            ->andWhere('w.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
